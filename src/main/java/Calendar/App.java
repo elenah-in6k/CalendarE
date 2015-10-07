b0c21ef20456291c85206cc103ba2b230bd78af6
ElenaH <elenah@interlink-ua.com>
2015-10-07 18:08:21 +0300

    refactoring after check

--- src/main/java/Calendar/App.java
+++ src/main/java/Calendar/App.java
@@ -1,110 +1,115 @@
 package Calendar;
-import java.io.*;
+
 import java.util.Calendar;
 
 public class App {
-	public static void main(String[] args) 	{
-		Calendar c = Calendar.getInstance();
-		System.out.println("DATE: " + c.getTime());
-		int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
-		int slide = dayOfMonth-c.get(Calendar.DAY_OF_WEEK)+1;
-		int qDaysOfMonthNow=QtyDaysOfMonth(c.get(Calendar.MONTH)+1,  c.get(Calendar.YEAR));
-		int qDaysOfMonthLast=QtyDaysOfMonth(c.get(Calendar.MONTH),  c.get(Calendar.YEAR));
-		int[][] calend;
-		calend = CalendarDays(qDaysOfMonthLast, slide, qDaysOfMonthNow);
-		PrintNameDaysOfWeek();
-		PrintCalendar (calend,  dayOfMonth);
-	}
+    public  static int weekSize = 7;
+    public  static int workWeekSize = 5;
+    public  static int qtyMonthWeek = 5;
+    public  static int nSut = 5;
+    public  static int nSun = 6;
+    public  static String blackText = (char) 27 + "[30m" ;
+    public  static String redText = (char) 27 + "[31m" ;
+    public  static String greenText = (char) 27 + "[32m" ;
+    public  static String yellowText = (char) 27 + "[33m" ;
+    public  static String blueText = (char) 27 + "[34m" ;
+    public  static String magentaText = (char) 27 + "[35m" ;
+    public  static String lightBlueText = (char) 27 + "[36m" ;
+    public  static String whiteText = (char) 27 + "[37m" ;
+    public  static String blackBackground = (char) 27 + "[40m" ;
+    public  static String redBackground = (char) 27 + "[41m" ;
+    public  static String greenBackground = (char) 27 + "[42m" ;
+    public  static String yellowBackground= (char) 27 + "[43m" ;
+    public  static String blueBackground= (char) 27 + "[44m" ;
+    public  static String magentaBackground= (char) 27 + "[45m" ;
+    public  static String lightBlueBackground = (char) 27 + "[46m" ;
+    public  static String whiteBackground= (char) 27 + "[47m" ;
+    public  static String boldText= (char) 27 + "[1m" ;
 
-	public static void PrintNameDaysOfWeek()	{
-		String[] dayWeekName;
-		dayWeekName = new String[8];
-		dayWeekName[1]="Mon";
-		dayWeekName[2]="Tue";
-		dayWeekName[3]="Wed";
-		dayWeekName[4]="Thu";
-		dayWeekName[5]="Fri";
-		dayWeekName[6]="Sut";
-		dayWeekName[7]="Sun";
-		for(int i=1; i<=7; i++)	{
-			System.out.print((char) 27 + "[46;31m"+dayWeekName[i]+" ");
-		}
-        	System.out.println();
-	}
 
-	public static int[][] CalendarDays (int qDaysOfMonthLast, int slide, int qDaysOfMonthNow) 	{
-		int[][] calend;
-		calend = new int [6][8];
-    		calend[1][slide-1]=qDaysOfMonthLast;
-		for (int i = slide-2; i>=1; i--)		{
-			calend [1][i] = calend[1][i+1]-1;
-		}
-		calend [1][slide]=1;
-		for (int i=slide+1; i<=7; i++) {
-			calend [1][i] = calend[1][i-1]+1;
-		}
-		for(int i=2; i<=5; i++)	 {
-			for(int j=1; j<=7;j++) {
-				if (calend[i][j-1]==qDaysOfMonthNow)	{
-					calend [i][j]=1;
-				}
-				else				{
-					if (j==1)					{
-						calend[i][j]= calend[i-1][7]+1;
-					}
-					else					{
-					calend[i][j]= calend[i][j-1]+1;
-					}
-				}
-			}
-		}
-		return calend;
-	}
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        System.out.println("DATE: " + c.getTime());
+        int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+        int firstDayOffset = dayOfMonth - c.get(Calendar.DAY_OF_WEEK) ;
+        int currentMonthSize = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+         c.set(c.MONTH - 1, 1);
+        int previousMonthSize = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+         int[][] monthCalendar = calendarDays(previousMonthSize, firstDayOffset, currentMonthSize);
+        printCalendarHeader();
+        printCalendarBody(monthCalendar, dayOfMonth);
+    }
 
-	public static int QtyDaysOfMonth(int numbMonth, int year)	{
-		if ((numbMonth == 1) | (numbMonth ==3 )| (numbMonth ==5) | (numbMonth ==7) | (numbMonth ==8) | (numbMonth ==10 )| (numbMonth ==12))		{
-			return  31;}
-		else if (numbMonth == 2)		{
-			if (((year%4==0)&(year%100!=0))||(year%400==0))			{
-				return 29;
-			}
-			else			{
-				return  28;
-			}
-		}
-		else 		{
-			return  30;}
+    public static void printCalendarHeader() {
+        String[] dayWeekName;
+        // edit with collection
+        dayWeekName = new String[7];
+        //edit array name day of week {,}
+        dayWeekName[0] = "Mon";
+        dayWeekName[1] = "Tue";
+        dayWeekName[2] = "Wed";
+        dayWeekName[3] = "Thu";
+        dayWeekName[4] = "Fri";
+        dayWeekName[5] = "Sut";
+        dayWeekName[6] = "Sun";
 
-	}
+        for (int i = 0; i <= 6; i++) {
+            System.out.print((char) 27 + "[46m" +redText + dayWeekName[i] + " "); //set sut sun else color
+        }
 
-	public static void PrintCalendar (int[][] calend, int dayOfMonth)	{
-		for (int i = 1; i <= 5; i++) {
-			for (int j = 1; j <= 7; j++) {
-				String space;
-				space = ((calend[i][j] <= 9) & (calend[i][j] >= 1)) ?  "   " :   "  ";
-				System.out.print((char) 27 + setColour(calend[i][j], i,  j,  dayOfMonth ) + calend[i][j] + space);
-			}
-			System.out.println();
-		}
-		System.out.println((char) 27+"[46m");
-	}
+        System.out.println();
+    }
 
-	public static String setColour(int day, int i, int j,  int dayOfMonth ) {
-		String colour;
-		if (((i == 1) & (day >= 26) & (day<= 31)) | ((i == 5) & (day < 26))) {
-			colour = "[37m";
-		}
-		else if (((j == 6) | (j == 7)) & (day != dayOfMonth)) {
-			colour = "[31m" ;
-		}
-		else if (day == dayOfMonth) {
-			colour = "[1;45;34m";
-		}
-		else {
-			colour = "[0m" ;
-		}
-		return colour;
-	}
+    public static int[][] calendarDays(int qDaysOfMonthLast, int firstDayOffset, int qDaysOfMonthNow) {
+        int[][] monthCalendar;
+        //extract const
+        monthCalendar = new int[qtyMonthWeek][weekSize];
+        monthCalendar[0][firstDayOffset] = qDaysOfMonthLast;
+        for (int i = firstDayOffset - 1; i >= 0; i--) {
+            monthCalendar[0][i] = monthCalendar[0][i + 1] - 1;
+        }
+        monthCalendar[0][firstDayOffset] = 1;
+        for (int i = firstDayOffset+1 ; i < weekSize; i++) {
+            monthCalendar[0][i] = monthCalendar[0][i - 1] + 1;
+        }
+        for (int i = 1; i <qtyMonthWeek; i++) {
+            for (int j = 0; j < weekSize; j++) {
+                if (j!=0){
+                     if  ((monthCalendar[i][j - 1] == qDaysOfMonthNow)|(monthCalendar[i-1][weekSize-1] == qDaysOfMonthNow) ) {
+                             monthCalendar[i][j] = 1;
+                     } else {
+                             monthCalendar[i][j] = monthCalendar[i][j - 1] + 1;
+                     }
+                } else {
+                        monthCalendar[i][j] = monthCalendar[i - 1][weekSize-1] + 1;
+                }
+            }
+        }
+        return monthCalendar;
+    }
 
-	
+     public static void printCalendarBody(int[][] monthCalendar, int dayOfMonth) {
+        for (int i = 0; i < qtyMonthWeek; i++) {
+            for (int j =0; j < weekSize; j++) {
+                System.out.print( getColour(monthCalendar[i][j], i, j, dayOfMonth) );
+                System.out.format("%4d", monthCalendar[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println(lightBlueBackground);
+    }
+
+    public static String getColour(int day, int i, int j, int dayOfMonth) {
+        String colour;
+        if (((i == 0) & (day >= 26) & (day <= 31)) | ((i == qtyMonthWeek - 1) & (day < 26))) {
+            colour = whiteText;
+        } else if (((j == nSut) | (j == nSun)) & (day != dayOfMonth)) {
+            colour = redText ;
+        } else if (day == dayOfMonth) {
+            colour =boldText + magentaBackground + blueText ;
+        } else {
+            colour = blackText ;
+        }
+        return colour;
+    }
 }
