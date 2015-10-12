d45c63fb9249c820eae1a866298728dad501bb54
ElenaH <elenah@interlink-ua.com>
2015-10-12 04:30:56 +0300

    create new method insideOutput Header and Body

--- src/main/java/Calendar/Printer.java
+++ src/main/java/Calendar/Printer.java
@@ -6,15 +6,33 @@ package Calendar;
 public abstract class Printer {
 
     abstract String[] color();
-    abstract void printCalendarBody();
-    abstract void printCalendarHeader();
+
+    abstract void insideOutputHeader(String dayName, int i);
+
+    abstract void insideOutputBody(MonthCalendar monthCalendar, int i);
+
+    public MonthCalendar monthCalendar;
+
+    void printCalendarHeader(MonthCalendar monthCalendar) {
+
+        for (int i = 1; i <= MonthCalendar.weekSize; i++) {
+            insideOutputHeader(monthCalendar.dayWeekName[i], i);
+        }
+    }
+
+    void printCalendarBody(MonthCalendar monthCalendar) {
+
+        for (int i = 0; i < MonthCalendar.monthWeekNumber; i++) {
+            insideOutputBody(monthCalendar, i);
+        }
+    }
 
     protected String getColorHeader(int dayOfWeek, String[] color) {
 
         return ((dayOfWeek <= MonthCalendar.workWeekSize)) ? color[0] : color[1];
     }
 
-    protected String getColourBody(int weekNumber, int dayOfWeek,MonthCalendar monthCalendar, String[] color) {
+    protected String getColourBody(int weekNumber, int dayOfWeek, MonthCalendar monthCalendar, String[] color) {
 
         if (monthCalendar.isOutOfMonth(weekNumber, dayOfWeek)) {
             return color[3];
@@ -25,7 +43,7 @@ public abstract class Printer {
         }
 
         if (monthCalendar.isCurrentDay(weekNumber, dayOfWeek)) {
-            return  color[2];
+            return color[2];
         }
 
         return color[0];
